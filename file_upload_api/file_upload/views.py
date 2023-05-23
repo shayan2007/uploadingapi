bc4b4e908a1e88adf7d8daa818223f104bd9f654
Shayan2007 <shayan.suhail@gmail.com>
2023-05-23 15:58:28 +0500

    v1

--- file_upload_api/file_upload/views.py
+++ file_upload_api/file_upload/views.py
@@ -1,14 +1,20 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UploadedFileSerializer
+
 
 class FileUploadView(APIView):
     def get(self, request, format=None):
         return render(request, 'file_upload/upload.html')
 
-    def post(self, request, format=None):
-        serializer = UploadedFileSerializer(data=request.data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        file = request.FILES['file']
+        file_storage = FileSystemStorage()
+        filename = file_storage.save(file.name, file)
+        file_url = file_storage.url(filename)
+
+        # Rest of your code
+
+        return Response({'file_url': file_url})
+
