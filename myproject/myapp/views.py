bca65783af2e58b4460eae826640c10a71312158
Shayan2007 <shayan.suhail@gmail.com>
2023-05-23 14:41:58 +0500

    v1

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,28 +1,27 @@
 # views.py
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class FileUploadView(APIView):
-    def post(self, request, format=None):
-        file_obj = request.FILES.get('file')
-        if file_obj is None:
-            return Response({'error': 'No file uploaded'}, status=400)
+    def get(self, request):
+        return render(request, 'upload.html')
 
-        # Validate the file (optional)
-        # Implement your file validation logic here
+    def post(self, request, format=None):
+        file_objs = request.FILES.getlist('files')
+        if len(file_objs) == 0:
+            return Response({'error': 'No files uploaded'}, status=400)
 
         try:
-            # Define the directory to save the file
-            save_dir = 'myproject/saved_files/'
+            save_dir = "C:\\Users\\Shayan'\\uploadingapi\\myproject\\saved_files"
 
-            # Construct the file path
-            file_path = save_dir + file_obj.name
+            for file_obj in file_objs:
+                file_path = save_dir + file_obj.name
 
-            # Save the file
-            with open(file_path, 'wb+') as destination:
-                for chunk in file_obj.chunks():
-                    destination.write(chunk)
+                with open(file_path, 'wb+') as destination:
+                    for chunk in file_obj.chunks():
+                        destination.write(chunk)
 
-            return Response({'message': 'File uploaded successfully'})
+            return Response({'message': 'Files uploaded successfully'})
         except Exception as e:
             return Response({'error': str(e)}, status=500)
